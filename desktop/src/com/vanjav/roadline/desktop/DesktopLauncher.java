50e59c6b26475ee699177dbc75289ccb96896ca4
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2018-04-17 16:18:52 -0400

    setup

---
+++ desktop/src/com/vanjav/roadline/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vanjav.roadline.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vanjav.roadline.Roadline;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Roadline(), config);
+	}
+}
