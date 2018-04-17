f4758d61b833f04222b5566f0a6f4721c895e4f1
Vanja Veselinovic <vanja.veselinovic.95@gmail.com>
2018-04-17 18:36:17 -0400

    set everything up, it works. at same point as before now

---
+++ core/src/com/vanjav/roadline/Controller.java
@@ -0,0 +1,308 @@
+package com.vanjav.roadline;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Random;
+
+/**
+ * Created by vveselin on 11/10/2016.
+ */
+
+public class Controller {
+    private LinkedList<PointF> linePoints, roadPoints;
+    private float score;
+
+    private int width, height;
+    private float crossTime;
+    private float lineWidth, roadWidth, outlineWidth;
+
+    private Random random;
+
+    /* zone 1 */
+    private LinkedList<TreePointF> treePoints;
+    private LinkedList<PointF> treePointsToRemove;
+    private float biggestTreeWidth;
+
+    public Controller (int width, int height) {
+        linePoints = new LinkedList<PointF>();
+        roadPoints = new LinkedList<PointF>();
+        firstLinePointToKeep = 0;
+        firstRoadPointToKeep = 0;
+        score = 0;
+
+        this.width = width;
+        this.height = height;
+        crossTime = 1; //1 second
+        lineWidth = 20;
+        roadWidth = 200;
+        outlineWidth = (float) (roadWidth * 1.75);
+        biggestTreeWidth = 300;
+
+        random = new Random();
+
+        /* zone 1* */
+        treePoints = new LinkedList<TreePointF>();
+        treePointsToRemove = new LinkedList<PointF>();
+
+        initRoad();
+        genRoad();
+    }
+
+    public LinkedList<PointF> getLinePoints() {
+        return linePoints;
+    }
+
+    public LinkedList<PointF> getRoadPoints() {
+        return roadPoints;
+    }
+
+    public LinkedList<TreePointF> getTreePoints() {
+        return treePoints;
+    }
+
+    public void addLinePoint(float x, float y) {
+        linePoints.add(new PointF(x, y));
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public float getLineWidth() {
+        return lineWidth;
+    }
+
+    public float getRoadWidth() {
+        return roadWidth;
+    }
+
+    public float getOutlineWidth() { return outlineWidth; }
+
+    private void updateScore(float x) {
+        score += x/1000.0;
+    }
+
+    private void initRoad() {
+        roadPoints.add(new PointF(0, height/2));
+        genItems(0, height/2);
+
+        roadPoints.add(new PointF(width/5, height/2));
+        genItems(width/5, height/2);
+
+        roadPoints.add(new PointF(2*width/5, height/2));
+        genItems(2*width/5, height/2);
+
+        roadPoints.add(new PointF(3*width/5, height/2));
+        genItems(3*width/5, height/2);
+
+        roadPoints.add(new PointF(4*width/5, height/2));
+        genItems(4*width/5, height/2);
+
+        roadPoints.add(new PointF(width, height/2));
+        genItems(width, height/2);
+
+        roadPoints.add(new PointF(width+width/2, height/2));
+        genItems(width+width/2, height/2);
+    }
+
+    private PointF lastPointInRoad;
+    private float plusMinus, genX, genY;
+
+    private void genRoad() {
+        lastPointInRoad = roadPoints.getLast();
+
+        while(lastPointInRoad.x < width*2) {
+            plusMinus = random.nextInt(2);
+
+            if (plusMinus == 0)
+                plusMinus = -1;
+
+            genX = random.nextFloat() * (lastPointInRoad.x+500-lastPointInRoad.x) + lastPointInRoad.x;
+            genY = plusMinus*random.nextFloat() * (lastPointInRoad.y+250-lastPointInRoad.y)+lastPointInRoad.y;
+
+            if (genY < 100)
+                genY = 100;
+            if (genY > height - 100)
+                genY = height - 100;
+
+            roadPoints.add(new PointF(genX, genY));
+            lastPointInRoad = roadPoints.getLast();
+
+            genItems(genX, genY);
+        }
+    }
+
+    private int left, right;
+
+    private int binarySearchTreePointListByY(LinkedList<TreePointF> list, float y) {
+        if (list.isEmpty()) return 0;
+
+        left = 0;
+        right = list.size() - 1;
+
+        while (right != left) {
+            if (y > list.get(left + (right-left)/2).y)
+                right = left + (right-left)/2;
+            else
+                left = left + (right-left)/2 + 1;
+        }
+
+        if (y < list.get(left).y) return left+1;
+        else return left;
+    }
+
+    private float addX, addY;
+    private int numItemsToAdd;
+    private int positionToInsert;
+    private int i;
+
+    private void genItems(float x, float y) {
+        //trees
+        numItemsToAdd = random.nextInt(5);
+
+        for (i = 0; i < numItemsToAdd; i++) {
+            addX = x-width/2 + random.nextFloat()*width/2;
+            addY = (float) (random.nextFloat()*1.1*height);
+
+            if (!isPointOnRoad(addX, addY)) {
+                positionToInsert = binarySearchTreePointListByY(treePoints, addY);
+                treePoints.add(positionToInsert, new TreePointF(addX, addY, random.nextInt(5)));
+            }
+        }
+    }
+
+    private PointF prevPoint, currPoint;
+    private float p1x, p1y, p2x, p2y, p3x, p3y, p4x, p4y;
+    private float k, m;
+    private int j;
+
+    private boolean isPointOnRoad(float x, float y) {
+        for (j = 1; j < roadPoints.size(); j++) {
+            prevPoint = roadPoints.get(j - 1);
+            currPoint = roadPoints.get(j);
+
+            if (x >= prevPoint.x - roadWidth && x <= currPoint.x + roadWidth) {
+                if (currPoint.y - prevPoint.y == 0) {
+                    p1x = prevPoint.x;
+                    p1y = prevPoint.y - roadWidth / 2;
+                    p2x = currPoint.x;
+                    p2y = currPoint.y - roadWidth / 2;
+                    p3x = currPoint.x;
+                    p3y = currPoint.y + roadWidth / 2;
+                    p4x = prevPoint.x;
+                    p4y = prevPoint.y + roadWidth / 2;
+                } else {
+                    m = -1 * ((currPoint.x - prevPoint.x) / (currPoint.y - prevPoint.y));
+                    k = (float) (roadWidth / (2 * Math.sqrt(1 + Math.pow(m, 2))));
+                    p1x = prevPoint.x + k;
+                    p1y = prevPoint.y + k * m;
+                    p2x = currPoint.x + k;
+                    p2y = currPoint.y + k * m;
+                    p3x = currPoint.x - k;
+                    p3y = currPoint.y - k * m;
+                    p4x = prevPoint.x - k;
+                    p4y = prevPoint.y - k * m;
+                }
+
+                float a, b, c, d1, d2, d3, d4;
+
+                a = -1 * (p2y - p1y);
+                b = p2x - p1x;
+                c = -1 * (a * p1x + b * p1y);
+                d1 = a * x + b * y + c;
+
+                a = -1 * (p3y - p2y);
+                b = p3x - p2x;
+                c = -1 * (a * p2x + b * p2y);
+                d2 = a * x + b * y + c;
+
+                a = -1 * (p4y - p3y);
+                b = p4x - p3x;
+                c = -1 * (a * p3x + b * p3y);
+                d3 = a * x + b * y + c;
+
+                a = -1 * (p1y - p4y);
+                b = p1x - p4x;
+                c = -1 * (a * p4x + b * p4y);
+                d4 = a * x + b * y + c;
+
+                if ((d1 >= 0 && d2 >= 0 && d3 >= 0 && d4 >= 0) || (d1 <= 0 && d2 <= 0 && d3 <= 0 && d4 <= 0))
+                    return true;
+
+                if (Math.pow(x - prevPoint.x, 2) + Math.pow(y - prevPoint.y, 2) <= Math.pow(100, 2))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int firstLinePointToKeep, firstRoadPointToKeep;
+    //private LinkedList<PointF> treePointsToRemove, flowerPointsToRemove;
+    private float offsetX;
+    private boolean pointToKeepFound = false;
+    private int l;
+
+    public boolean update(float deltaTimeSeconds) {
+        genRoad();
+
+        updateScore((float) width*(deltaTimeSeconds/crossTime));
+
+        offsetX = -1*width*(deltaTimeSeconds/crossTime);
+
+        if (roadPoints.size() > 0) {
+            roadPoints.getFirst().offset(offsetX, 0);
+            firstRoadPointToKeep = 0;
+
+            for (l = 1; l < roadPoints.size(); l++) {
+                currPoint = roadPoints.get(l);
+                currPoint.offset(offsetX, 0);
+
+                if (currPoint.x < 0 - outlineWidth)
+                    firstRoadPointToKeep = l - 1;
+            }
+
+            roadPoints.subList(0, firstRoadPointToKeep).clear();
+        }
+
+        if (treePoints.size() > 0) {
+            treePointsToRemove.clear();
+
+            for (l = 0; l < treePoints.size(); l++) {
+                currPoint = treePoints.get(l);
+                currPoint.offset(offsetX, 0);
+
+                if (currPoint.x < -1 * width - biggestTreeWidth)
+                    treePointsToRemove.add(currPoint);
+            }
+
+            treePoints.removeAll(treePointsToRemove);
+        }
+
+        if (linePoints.size() > 0) {
+            pointToKeepFound = false;
+
+            linePoints.getFirst().offset(offsetX, 0);
+            firstLinePointToKeep = 0;
+
+            for (l = 1; l < linePoints.size(); l++) {
+                currPoint = linePoints.get(l);
+                currPoint.offset(offsetX, 0);
+
+                if (!pointToKeepFound) {
+                    if (currPoint.x < 0 - lineWidth) {
+                        firstLinePointToKeep = l - 1;
+                    } else {
+                        pointToKeepFound = true;
+                    }
+                }
+            }
+
+            linePoints.subList(0, firstLinePointToKeep).clear();
+
+            return isPointOnRoad(linePoints.getLast().x, linePoints.getLast().y);
+        }
+
+        return false;
+    }
+}
--- core/src/com/vanjav/roadline/Roadline.java
+++ core/src/com/vanjav/roadline/Roadline.java
@@ -2,32 +2,197 @@ package com.vanjav.roadline;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.Color;
+
+import java.util.LinkedList;
 
 public class Roadline extends ApplicationAdapter {
+	private int width;
+	private int height;
+	private long previousFrameNanos;
+	private float currX, currY;
+
+	private boolean paused = false;
+	private boolean gameStarted = false;
+	private boolean gameOver = false;
+
+	private PointF prevPoint, currPoint;
+	private int i;
+
+	private Controller controller;
+	private float roadWidth, outlineWidth, lineWidth;
+	private Color bgColor, outlineColor, roadColor, lineColor;
+
+	private LinkedList<Texture> treeTextures;
+
 	SpriteBatch batch;
-	Texture img;
-	
+	BitmapFont font;
+	ShapeRenderer shapeRenderer;
+	Texture treeTexture;
+
 	@Override
 	public void create () {
+	    width = Gdx.graphics.getWidth();
+	    height =  Gdx.graphics.getHeight();
+
+        controller = new Controller(width, height);
+
+        roadWidth = controller.getRoadWidth();
+        outlineWidth = controller.getOutlineWidth();
+        lineWidth = controller.getLineWidth();
+
+        bgColor = new Color(0.459F, 0.714F, 0.282F, 1);
+        outlineColor = new Color(0.431F, 0.686F, 0.255F, 1);
+        roadColor = new Color(0.565F, 0.565F, 0.565F, 1);
+        lineColor = new Color(0.929f, 0.765f, 0.271f,1);
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		shapeRenderer = new ShapeRenderer();
+		treeTexture = new Texture(Gdx.files.internal("tree.png"));
+
+		treeTextures = new LinkedList<Texture>();
+
+		Pixmap pixmapOriginal = new Pixmap(Gdx.files.internal("tree.png"));
+		Pixmap pixmapResized = null;
+		for (i = 0; i < 5; i++) {
+			pixmapResized = new Pixmap(
+					100 + 50*i,
+					(int) ((100 + 50*i) * ((float) pixmapOriginal.getHeight() / (float) pixmapOriginal.getWidth())),
+					pixmapOriginal.getFormat());
+
+			treeTextures.add(new Texture(pixmapResized));
+		}
+		pixmapOriginal.dispose();
+		pixmapResized.dispose();
+
+		Gdx.input.setInputProcessor(new InputAdapter(){
+			@Override
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+				if (gameOver) return false;
+
+				currX = screenX;
+				currY = height - screenY;
+
+				gameStarted = true;
+				return true;
+			}
+
+			@Override
+			public boolean touchDragged(int screenX, int screenY, int pointer) {
+				currX = screenX;
+				currY = height - screenY;
+
+				return true;
+			}
+
+			@Override
+			public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+				gameOver();
+
+				return true;
+			}
+		});
+	}
+
+	public void gameOver() {
+		if (gameStarted) {
+			gameOver = true;
+			// show restart menu
+		}
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		update(Gdx.graphics.getDeltaTime());
+		draw();
+	}
+
+	private void update(float deltaTimeSeconds) {
+		if (gameStarted && !gameOver) {
+			controller.addLinePoint(currX, currY);
+
+			if (!controller.update(deltaTimeSeconds)) {
+				gameOver();
+			}
+		}
+	}
+
+	private String score;
+	private Texture currTree;
+
+	private void draw() {
+		Gdx.gl.glClearColor(bgColor.r, bgColor.g, bgColor.b, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+		shapeRenderer.setColor(outlineColor.r, outlineColor.g, outlineColor.b, 1);
+		for (i = 1; i < controller.getRoadPoints().size(); i++) {
+			prevPoint = controller.getRoadPoints().get(i-1);
+			currPoint = controller.getRoadPoints().get(i);
+			if (prevPoint.x < width + outlineWidth) {
+				shapeRenderer.rectLine(prevPoint.x, prevPoint.y, currPoint.x, currPoint.y, outlineWidth);
+				shapeRenderer.circle(prevPoint.x, prevPoint.y, outlineWidth/2);
+			}
+
+		}
+
+		shapeRenderer.setColor(roadColor.r, roadColor.g, roadColor.b, 1);
+		for (i = 1; i < controller.getRoadPoints().size(); i++) {
+			prevPoint = controller.getRoadPoints().get(i-1);
+			currPoint = controller.getRoadPoints().get(i);
+			if (prevPoint.x < width + roadWidth) {
+				shapeRenderer.rectLine(prevPoint.x, prevPoint.y, currPoint.x, currPoint.y, roadWidth);
+				shapeRenderer.circle(prevPoint.x, prevPoint.y, roadWidth / 2);
+			}
+		}
+
+		shapeRenderer.setColor(lineColor.r, lineColor.g, lineColor.b, 1);
+		for (i = 1; i < controller.getLinePoints().size(); i++) {
+			prevPoint = controller.getLinePoints().get(i-1);
+			currPoint = controller.getLinePoints().get(i);
+			if (prevPoint.x < width + lineWidth) {
+				shapeRenderer.rectLine(prevPoint.x, prevPoint.y, currPoint.x, currPoint.y, lineWidth);
+				shapeRenderer.circle(prevPoint.x, prevPoint.y, lineWidth / 2);
+			}
+		}
+
+		shapeRenderer.end();
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		for (i = 1; i < controller.getTreePoints().size(); i++) {
+			currPoint = controller.getTreePoints().get(i);
+			currTree = treeTextures.get(((TreePointF) currPoint).size);
+
+			if (currPoint.x < width + treeTexture.getWidth())
+				batch.draw(treeTexture, currPoint.x - treeTexture.getWidth()/2, currPoint.y - treeTexture.getHeight());
+		}
+
+		score = ""+Math.round(controller.getScore()*10.0)/10.0;
+
+		//canvas.drawText(score, width/2 + 2, 200 + 2, paintStroke);
+		//canvas.drawText(score, width/2, 200, paintText);
+
+		/*
+		canvas.drawText("line points: "+controller.getLinePoints().size(), 50, 50, paintDebug);
+		canvas.drawText("road points: "+controller.getRoadPoints().size(), 50, 100, paintDebug);
+		canvas.drawText("tree points: "+controller.getTreePoints().size(), 50, 200, paintDebug);
+		*/
+
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
